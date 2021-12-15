7830180f432701e720ae04b525a99b87d34dd359
im-moo <54445570+im-moo@users.noreply.github.com>
2021-12-15 22:24:14 +0800

    更新

---
+++ SimpleMVVM/SimpleCommand.cs
@@ -0,0 +1,101 @@
+﻿using System.Windows.Input;
+
+namespace SimpleMVVM
+{
+    public class SimpleCommand : ICommand
+    {
+        private readonly Action _execute;
+
+        private readonly Func<bool> _canExecute;
+
+        private readonly List<WeakReference> _eventHandlers = new();
+
+        private readonly object _syncRoot = new();
+
+        public SimpleCommand(Action execute) : this(execute, null)
+        {
+        }
+
+        public SimpleCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                {
+                    lock (_syncRoot)
+                    {
+                        _eventHandlers.Add(new WeakReference(value));
+                    }
+                }
+            }
+
+            remove
+            {
+                if (_canExecute != null)
+                {
+                    lock (_syncRoot)
+                    {
+                        foreach (var thing in _eventHandlers)
+                        {
+                            var target = thing.Target;
+                            if (target != null && (EventHandler)target == value)
+                            {
+                                _eventHandlers.Remove(thing);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<EventHandler> SafeCopyEventHandlerList()
+        {
+            lock (_syncRoot)
+            {
+                var toReturn = new List<EventHandler>();
+                var deadEntries = new List<WeakReference>();
+
+                foreach (var thing in _eventHandlers)
+                {
+                    if (!thing.IsAlive)
+                    {
+                        deadEntries.Add(thing);
+                        continue;
+                    }
+
+                    if (thing.Target is EventHandler eventHandler)
+                    {
+                        toReturn.Add(eventHandler);
+                    }
+                }
+
+                foreach (var weakReference in deadEntries)
+                {
+                    _eventHandlers.Remove(weakReference);
+                }
+
+                return toReturn;
+            }
+        }
+
+        public void RaiseCanExecuteChanged(object sender)
+        {
+            var list = SafeCopyEventHandlerList();
+            foreach (var eventHandler in list)
+            {
+                eventHandler(sender, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+
+        public void Execute(object parameter) => _execute();
+    }
+}
---
+++ SimpleMVVM/ViewModelBase.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimpleMVVM
+{
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        private bool disposedValue;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                storage = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                // TODO: 将大型字段设置为 null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        // ~ViewModelBase()
+        // {
+        //     // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+        //     Dispose(disposing: false);
+        // }
+
+        void IDisposable.Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
