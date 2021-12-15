f975ffc5c363ca407a8a05a41e07cd7955ce75a4
im-moo <54445570+im-moo@users.noreply.github.com>
2021-12-15 22:16:01 +0800

    添加项目文件。

---
+++ SimpleMVVMDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿using SimpleMVVM;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleMVVMDemo.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _time = "00:00:00.0000000";
+        private bool _clockIsRunning;
+
+        public string Time
+        {
+            get
+            {
+                return _time;
+            }
+            set => SetProperty(ref _time, value);
+        }
+
+        public async Task StartClock()
+        {
+            ClockIsRunning = true;
+
+            await Task.Run(async () =>
+             {
+                 while (ClockIsRunning)
+                 {
+                     await Task.Delay(1);
+
+                     if (ClockIsRunning)
+                     {
+                         Time = DateTime.Now.ToString("HH:mm:ss.fffffff");
+                     }
+                 }
+             });
+        }
+
+        public void StopClock()
+        {
+            ClockIsRunning = false;
+        }
+
+        private SimpleCommand start;
+        public ICommand Start => start ??= new SimpleCommand(async () => { await PerformStart(); });
+
+        private async Task PerformStart()
+        {
+            await StartClock();
+        }
+
+        private SimpleCommand stop;
+        public ICommand Stop => stop ??= new SimpleCommand(() => PerformStop(new object()));
+
+        public bool ClockIsRunning
+        {
+            get => _clockIsRunning;
+            set
+            {
+                _clockIsRunning = value;
+                OnPropertyChanged(nameof(ClockIsRunning));
+            }
+        }
+
+        private void PerformStop(object obj)
+        {
+            StopClock();
+        }
+    }
+}
