d2ff17be01dbe3f7ae8d322066d790ec89490dc7
im-moo <54445570+im-moo@users.noreply.github.com>
2021-12-18 00:28:47 +0800

    新增密码绑定

--- SimpleMVVMDemo/ViewModel/HomeViewModel.cs
+++ SimpleMVVMDemo/ViewModel/HomeViewModel.cs
@@ -2,10 +2,7 @@
 using SimpleMVVM;
 using SimpleMVVMDemo.Views;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
---
+++ SimpleMVVMDemo/ViewModel/SignInViewModel.cs
@@ -0,0 +1,40 @@
+﻿using SimpleMVVM;
+using System.Security;
+using System.Windows;
+
+namespace SimpleMVVMDemo.ViewModel
+{
+    public class SignInViewModel : ViewModelBase
+    {
+
+        private string account;
+
+        public string Account { get => account; set => SetProperty(ref account, value); }
+
+        private string password;
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public static SecureString GetEncryptedPassword(DependencyObject obj)
+        {
+            return (SecureString)obj.GetValue(EncryptedPasswordProperty);
+        }
+
+        public static void SetEncryptedPassword(DependencyObject obj, SecureString value)
+        {
+            obj.SetValue(EncryptedPasswordProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for EncryptedPassword.  This enables animation, styling, binding, etc...
+        public static DependencyProperty EncryptedPasswordProperty =
+            DependencyProperty.RegisterAttached("EncryptedPassword", typeof(SecureString), typeof(SignInViewModel));
+    }
+}
