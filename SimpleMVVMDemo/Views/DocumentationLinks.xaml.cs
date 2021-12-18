d72b24e576b28611be3b0340545c68d48d7e5763
im-moo <54445570+im-moo@users.noreply.github.com>
2021-12-18 14:12:04 +0800

    搜索

---
+++ SimpleMVVMDemo/Views/DocumentationLinks.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SimpleMVVMDemo.Views
+{
+    /// <summary>
+    /// Interaction logic for DocumentationLinks.xaml
+    /// </summary>
+    public partial class DocumentationLinks : UserControl
+    {
+        public DocumentationLinks()
+        {
+            InitializeComponent();
+        }
+    }
+}
