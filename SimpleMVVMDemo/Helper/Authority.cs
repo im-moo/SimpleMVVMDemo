603458dd1128fd7392dea892ec04c403e255716a
汤姆 克鲁斯 <v5.mo@icloud.com>
2021-12-24 17:44:34 +0800

    save

---
+++ SimpleMVVMDemo/Helper/Authority.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SimpleMVVMDemo.Helper
+{
+    public class Authority
+    {
+        public static List<Visibility> AuthorityOperating(int authority, int count)
+        {
+            var visibilities = new List<Visibility>();
+            for (int i = 0; i <= count; i++)
+            {
+                int temp = 1 << i;
+                visibilities.Add((authority & temp) == temp ? Visibility.Visible : Visibility.Collapsed);
+            }
+            return visibilities;
+        }
+    }
+}
