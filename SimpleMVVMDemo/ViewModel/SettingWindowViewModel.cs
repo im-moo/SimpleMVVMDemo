a7daba4d9091e5544acd843af3b13be4f88211b5
im-moo <54445570+im-moo@users.noreply.github.com>
2022-01-09 21:44:46 +0800

    save

---
+++ SimpleMVVMDemo/ViewModel/SettingWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SimpleMVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMVVMDemo.ViewModel
+{
+    public class SettingWindowViewModel:ViewModelBase
+    {
+    }
+}
