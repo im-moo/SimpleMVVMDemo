50e28a43a468c19d3c7e13387e1a6e02c1c43cae
im-moo <54445570+im-moo@users.noreply.github.com>
2022-01-09 21:30:12 +0800

    保存

---
+++ SimpleMVVMDemo/ViewModel/ADD/StructureViewModel.cs
@@ -0,0 +1,39 @@
+﻿using SimpleMVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleMVVMDemo.ViewModel
+{
+    public class StructureViewModel : ViewModelBase
+    {
+
+        private SimpleCommand clickSetting;
+        public ICommand ClickSetting => clickSetting ??= new SimpleCommand(PerformClickSetting);
+
+        private void PerformClickSetting()
+        {
+        }
+
+        private System.Collections.IEnumerable movieCategories;
+
+        public System.Collections.IEnumerable MovieCategories { get => movieCategories; set => SetProperty(ref movieCategories, value); }
+
+        private SimpleCommand addCommand;
+        public ICommand AddCommand => addCommand ??= new SimpleCommand(Add);
+
+        private void Add()
+        {
+        }
+
+        private SimpleCommand removeSelectedItemCommand;
+        public ICommand RemoveSelectedItemCommand => removeSelectedItemCommand ??= new SimpleCommand(RemoveSelectedItem);
+
+        private void RemoveSelectedItem()
+        {
+        }
+    }
+}
---
+++ SimpleMVVMDemo/Views/ADD/OperationAuthority.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleMVVMDemo.Views
+{
+    /// <summary>
+    /// OperationAuthority.xaml 的交互逻辑
+    /// </summary>
+    public partial class OperationAuthority : Window
+    {
+        public OperationAuthority()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+        }
+    }
+}
